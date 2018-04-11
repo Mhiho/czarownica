c1f7879e5dfd8ad8a3b442a372d752f754af955c
Mhiho <m.pelka7@gmail.com>
2018-04-11 07:56:31 +0200

    poczatek

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div>Zacznijmy</div>
+    );
+  }
+}
+
+export default App;
